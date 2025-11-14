49a1d717e17a9fa883f0d43435a950c343db21f1
Sevag Bairamian <163696080+Sbairamian@users.noreply.github.com>
2025-11-13 23:07:02 -0800

    Add files via upload

---
+++ Scripts/Powershell Scripts/Change local password.ps1
@@ -0,0 +1,3 @@
+$accountname = Read-Host "Please enter the username for the password you want to change"
+$NewPassword = Read-Host -AsSecureString "Enter the temporary password for the user"
+Set-LocalUser -Name $accountname -Password $NewPassword
