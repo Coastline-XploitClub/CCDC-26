f1d76975a3b9de225f249bb571ced08b672e8b80
Sevag Bairamian <163696080+Sbairamian@users.noreply.github.com>
2025-12-05 19:29:37 -0800

    Update Change local password.ps1

--- Scripts/Powershell Scripts/Change local password.ps1
+++ Scripts/Powershell Scripts/Change local password.ps1
@@ -1,3 +1,6 @@
+#Use if you want to.  If not, just use GUI instead.
+
 $accountname = Read-Host "Please enter the username for the password you want to change"
 $NewPassword = Read-Host -AsSecureString "Enter the temporary password for the user"
+
 Set-LocalUser -Name $accountname -Password $NewPassword
