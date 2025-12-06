f0f4f6b579d0f2d9e629d291d279fef7e2e63298
Sevag Bairamian <163696080+Sbairamian@users.noreply.github.com>
2025-12-05 19:51:05 -0800

    Delete Scripts/Competition Ready directory

--- Scripts/Competition Ready/change pass.ps1
+++
@@ -1,3 +0,0 @@
-﻿$accountname = Read-Host "Please enter the username for the password you want to change"
-$NewPassword = Read-Host -AsSecureString "Enter the temporary password for the user"
-Set-ADAccountPassword -Identity $accountname -NewPassword $NewPassword -Reset
